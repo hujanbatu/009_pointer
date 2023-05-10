bb28eeccb62d040b042d018c4e5cc5f452073f9d
hujanbatu <haikal04102001p@gmail.com>
2023-05-10 12:22:50 +0700

    first commit

---
+++ pointer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+ int nim;
+ void showNim(); //deklarasi method
+};
+
+
+void mahasiswa::showNim() { //implementasi method diluar class
+ cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+ mahasiswa mhs{ 1 };         //object mhs
+ mhs.showNim();              //Member Acces Operator
+
+ mahasiswa& ref = mhs;    //Pointer Reference refMhs
+ ref.nim = 2;             //Member Access Operator
+ mhs.showNim();
+
+ mahasiswa* pMhs = &mhs;     //Pointer Dereference pMhs
+ pMhs->nim = 3;              //Arrow Operator
+ mhs.showNim();
+ system("pause");
+ return 0;
+}
